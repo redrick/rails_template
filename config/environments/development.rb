e047befa5699282c8c33e753db6c7f6dd41af2be
Andrej Antas <andrej@antas.cz>
2017-09-01 14:55:42 +0200

    Enable web-console and translation missing raise in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Raises error for missing translations
+  config.action_view.raise_on_missing_translations = true
+  config.web_console.whitelisted_ips = '0.0.0.0/0'
 end
